eab075023f6c2439a315012c76671d2787899cee
John <john@trabian.com>
2020-04-26 14:59:14 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React, {useReducer} from 'react';
-import './App.css';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import "./App.css";
+import styled from "styled-components";
 
-const clicksToSolve = 3;
+const mod4Mode = false;
+const mod = mod4Mode ? 4 : 2;
+const boardSize = 6;
+const boardDensity = 0.4;
+const cellCount = boardSize * boardSize;
+const lightsOnCount = Math.floor(cellCount * boardDensity);
 
 const MainGrid = styled.div`
   padding: 20px;
@@ -10,122 +15,130 @@ const MainGrid = styled.div`
   height: calc(100vh - 100px);
   width: calc(100vw - 40px);
   gap: 3px;
-  grid-template-columns: repeat(${props => props.columns}, 1fr);
-  grid-template-rows: repeat(${props => props.rows}, 1fr);
+  grid-template-columns: repeat(${(props) => props.size}, 1fr);
+  grid-template-rows: repeat(${(props) => props.size}, 1fr);
 `;
 
 const Cell = styled.div`
   height: 1fr;
   width: 1fr;
-  box-shadow: 0 0 20px rgba(0,0,0,0.9);
-  background: ${props => {
-    if(props.cell.clicks % (clicksToSolve + 1) === 0) {
-      return (
-      "linear-gradient(to bottom, #434343, #000000)");
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.9);
+  background: ${(props) => {
+    const timesClicked = props.cell.clicks;
+    if ((timesClicked - 0) % mod === 0) {
+      return "linear-gradient(to bottom, #434343, #000000)";
     }
-    if((props.cell.clicks - 1) % (clicksToSolve + 1) === 0) {
-      return ("linear-gradient(to top, #fffc00, #fff)");
+    if ((timesClicked - 1) % mod === 0) {
+      return "linear-gradient(to top, #fffc00, #fff)";
     }
-    if((props.cell.clicks - 2) % (clicksToSolve + 1) === 0) {
-      return ("linear-gradient(to bottom, #fc6767, #ec008c)");
+    if ((timesClicked - 2) % mod === 0) {
+      return "linear-gradient(to bottom, #fc6767, #ec008c)";
     }
-    if((props.cell.clicks - 3) % (clicksToSolve + 1) === 0) {
-      return ("linear-gradient(to bottom, #56ccf2, #2f80ed)");
+    if ((timesClicked - 3) % mod === 0) {
+      return "linear-gradient(to bottom, #56ccf2, #2f80ed)";
     }
   }};
   border-radius: 5px;
 `;
 
+const getPosition = (id, rowSize) => {
+  const row = Math.floor(id / rowSize);
+  const column = id % rowSize;
+  return { column, row };
+};
 
-const reducer = (state, {type, payload}) => {
-  let columns = state.columns;
-  let rows = state.rows;
-
-  switch (type) {
-    case "handleCellClick":
-      let {position, id} = payload;
-      let cellsToFlip = []
-      cellsToFlip.push(id)
-      if(position.column !== columns - 1) {
-        cellsToFlip.push(id + 1)
-      }
-      if(position.column !== 0) {
-        cellsToFlip.push(id - 1)
-      }
-      if(position.row !== rows - 1) {
-        cellsToFlip.push(id + columns)
-      }
-      if(position.row !== 0 ) {
-        cellsToFlip.push(id - columns)
-      }
-      let newCells = [
-        ...state.cells.filter(cell => !cellsToFlip.includes(cell.id)),
-        ...state.cells.filter(cell => cellsToFlip.includes(cell.id))
-        .map(cell => ({...cell, clicks: cell.clicks + 1 })),
-       ].sort((a, b) => a.id - b.id)
-       return {...state, cells: newCells};
-    default:
-      return state;
-  }
-}
-
-
-
-const initialState = () => {
-  let columns = 6;
-  let rows = 6;
-  let cells = [];
-  let numberLit = 15;
-  
-  const shuffle = (array) => {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-      while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
-  }
-  let litCells = shuffle([...Array((rows * columns)).keys()]).slice(0, numberLit);
-  
-  let rowCount;
-  for(rowCount = 0; rowCount < rows; rowCount++) {
-    let columnCount;
-    for(columnCount = 0; columnCount < columns; columnCount++) {
-      cells.push(
-        {
-          id: (rowCount * columns + columnCount) , 
-          position: {column: columnCount, row:rowCount},
-          clicks: litCells.includes(rowCount * columns + columnCount) ? 1 : 0,
-        }
-      );
+const getSolution = (lightsOnCount, cells) => {
+  const length = cells.length;
+  let solution = [];
+  while (solution.length < lightsOnCount) {
+    const randomPosition = Math.round(Math.random() * length);
+    if (!solution.includes(randomPosition)) {
+      solution.push(randomPosition);
     }
   }
-  return({
-    columns: columns, 
-    rows: rows, 
-    cells: cells,
+  return solution;
+};
+
+const getSolvedBoard = () => {
+  return new Array(cellCount).fill(0).map((_newCell, ci) => {
+    return {
+      id: ci,
+      position: getPosition(ci, boardSize),
+      clicks: 0,
+      solution: 0,
+    };
   });
 };
 
+const clickCell = (cell, cells) => {
+  const { position, id } = cell;
+  let cellsToFlip = [];
+  cellsToFlip.push(id);
+  if (position.column !== boardSize - 1) {
+    cellsToFlip.push(id + 1);
+  }
+  if (position.column !== 0) {
+    cellsToFlip.push(id - 1);
+  }
+  if (position.row !== boardSize - 1) {
+    cellsToFlip.push(id + boardSize);
+  }
+  if (position.row !== 0) {
+    cellsToFlip.push(id - boardSize);
+  }
+  const newCells = [
+    ...cells.filter((cell) => !cellsToFlip.includes(cell.id)),
+    ...cells
+      .filter((cell) => cellsToFlip.includes(cell.id))
+      .map((cell) => ({ ...cell, clicks: cell.clicks + 1 })),
+  ].sort((a, b) => a.id - b.id);
+  return newCells;
+};
+const getInitialCells = (solvedCells, solution) => {
+  let initialCells = [...solvedCells];
+  solution.forEach((cell, _solutionCellIndex) => {
+    initialCells = clickCell(solvedCells[cell], initialCells);
+    if (mod4Mode) {
+      initialCells = clickCell(solvedCells[cell], initialCells);
+    }
+  });
+  return initialCells.map((cell, cellIndex) => {
+    return { ...cell, solution: solution.includes(cellIndex) ? 1 : 0 };
+  });
+};
+
+const solvedBoard = getSolvedBoard();
+const solution = getSolution(lightsOnCount, solvedBoard);
+const initialCells = getInitialCells(solvedBoard, solution);
 const App = () => {
-  const [{
-    columns, 
-    rows,
-    cells,
-  }, dispatch] = useReducer(reducer, initialState());
-  return(
-    <>
-      <MainGrid columns={columns} rows={rows} >
-        {cells.map(cell => (
-          <Cell key={cell.id} cell={cell} onClick={() => dispatch({type: "handleCellClick", payload: cell})} />
-        ))}
-      </MainGrid>
-      
-    </>
+  const [cells, setCells] = useState(initialCells);
+  const handleCellClick = (cell) => {
+    setCells(clickCell(cell, cells));
+  };
+  console.warn({ cells, solution });
+  return (
+    <MainGrid size={boardSize}>
+      {cells.map((cell) => (
+        <Cell
+          key={cell.id}
+          cell={cell}
+          style={{ fontSize: "3rem" }}
+          onClick={() => handleCellClick(cell)}
+        >
+          <div
+            style={{
+              diplay: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            {/* <div>{cell.clicks % mod}</div> */}
+            <div>{cell.solution}</div>
+          </div>
+        </Cell>
+      ))}
+    </MainGrid>
   );
-}
+};
 
 export default App;
